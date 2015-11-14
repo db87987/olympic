169f2d897be61061347e99dc136055186f7be6dc
Dragon Black <db87987@gmail.com>
2015-11-14 12:51:51 +0300

    Updates local resource image sizes

--- app/models/local_resource.rb
+++ app/models/local_resource.rb
@@ -1,4 +1,4 @@
 class LocalResource < ActiveRecord::Base
-  has_attached_file :image, styles: { icon: "90x", front: "250x" }
+  has_attached_file :image, styles: { icon: "90x69#", front: "250x79#" }
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
